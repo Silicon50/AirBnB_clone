b9b533c9c2659c15f1ffd9c1dc378908461de1a8
Silicon50 <ndukwechukwuaja@gmail.com>
2023-08-13 22:27:53 +0100

    model

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,10 @@ from datetime import datetime
 
 class BaseModel():
     """A base class that define common attributes and methods of other classes
-    Attribute: 
+    Attribute:
         id : string - returns a unique identifier when an instance is created
-        created_at : datetime - assign current datetime when an instance is created
-        updated_at : datetime - assign with current datetime when an instance is created
-    
+        created_at : datetime - assign current datetime
+        updated_at : datetime - assign with current datetime
     Public Instance Methods :
         save : update update_at with current datetime
         to_dict : returns a dictionay containing all keys/value of the instance
@@ -26,10 +25,11 @@ class BaseModel():
         if len(kwargs) != 0:
             for key, value in kwargs.item():
                 if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__[key] = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = value
-        else:                
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -40,11 +40,11 @@ class BaseModel():
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
-        """A method used to update the updtated_at withe the current datetime"""
+        """A method used to update the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """Returns a dict keys/values of __dict__ of the instance"""
         self.__dict__["__class__"] = self.__class__.__name__
         self.__dict__["created_at"] = self.created_at.isoformat()
         self.__dict__["updated_at"] = self.updated_at.isoformat()
