0bd601c9b0e693723063ae4006f307bcc08c2dac
Silicon50 <ndukwechukwuaja@gmail.com>
2023-08-16 03:30:58 +0100

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -23,7 +23,7 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """Initializing the basemodel"""
         if len(kwargs) != 0:
-            for key, value in kwargs.item():
+            for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(
                         value, "%Y-%m-%dT%H:%M:%S.%f")
@@ -50,3 +50,25 @@ class BaseModel():
         dic["created_at"] = self.created_at.isoformat()
         dic["updated_at"] = self.updated_at.isoformat()
         return dic
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
